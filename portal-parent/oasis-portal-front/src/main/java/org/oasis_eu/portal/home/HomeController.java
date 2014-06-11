7969444877e7a5b052223899910134865c682071
Sylvain Chambon <sylvain.chambon@openwide.fr>
2014-06-11 15:11:37 +0200

    Display home page content

--- portal-parent/oasis-portal-front/src/main/java/org/oasis_eu/portal/home/HomeController.java
+++ portal-parent/oasis-portal-front/src/main/java/org/oasis_eu/portal/home/HomeController.java
@@ -1,15 +1,21 @@
 package org.oasis_eu.portal.home;
 
+import org.oasis_eu.portal.core.services.cms.ContentRenderingService;
 import org.oasis_eu.portal.generic.PortalController;
 import org.oasis_eu.spring.kernel.security.OpenIdCAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.RequestContext;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Locale;
 
 /**
  * User: schambon
@@ -17,6 +23,10 @@ import java.net.UnknownHostException;
  */
 @Controller
 public class HomeController extends PortalController {
+
+    @Autowired
+    private ContentRenderingService contentRenderingService;
+
 
     @ModelAttribute("username")
     public String username() {
@@ -36,7 +46,11 @@ public class HomeController extends PortalController {
         }
     }
 
-
+    @ModelAttribute("home_content")
+    public String content(HttpServletRequest request) {
+        Locale locale = RequestContextUtils.getLocale(request);
+        return contentRenderingService.render("home", locale);
+    }
 
     @RequestMapping("/")
     public String index() {
