7d89ee29bb9c787a81121ab963e5109bfa98810e
Marwane Kalam-Alami <marwane.ka@gmail.com>
2014-06-24 10:08:01 +0200

    #2: use custom dialect to embed content with a single attribute

--- portal-parent/oasis-portal-front/src/main/java/org/oasis_eu/portal/config/OasisWebConfiguration.java
+++ portal-parent/oasis-portal-front/src/main/java/org/oasis_eu/portal/config/OasisWebConfiguration.java
@@ -1,6 +1,12 @@
 package org.oasis_eu.portal.config;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.oasis_eu.portal.dialect.CMSDialect;
 import org.oasis_eu.spring.kernel.security.TokenRefreshInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -8,9 +14,9 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 /**
  * User: schambon
@@ -18,6 +24,15 @@ import java.util.Locale;
  */
 @Configuration
 public class OasisWebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    ITemplateResolver defaultTemplateResolver;
+
+    @Autowired
+    CMSDialect cmsDialect;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -34,5 +49,15 @@ public class OasisWebConfiguration extends WebMvcConfigurerAdapter {
         // Do not set a default locale - this will fall back to using the request's Accept header
 
         return clr;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(defaultTemplateResolver);
+        Set<IDialect> additionalDialects = new HashSet<IDialect>();
+        additionalDialects.add(cmsDialect);
+        springTemplateEngine.setAdditionalDialects(additionalDialects);
+        return springTemplateEngine;
     }
 }
