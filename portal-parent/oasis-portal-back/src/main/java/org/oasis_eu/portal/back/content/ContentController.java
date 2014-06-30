b1ecff13597d64f05eff69ff4a5b53794c2ad735
Marwane Kalam-Alami <marwane.ka@gmail.com>
2014-06-30 17:32:24 +0200

    #1: implement content rename, th: -> data-th-

--- portal-parent/oasis-portal-back/src/main/java/org/oasis_eu/portal/back/content/ContentController.java
+++ portal-parent/oasis-portal-back/src/main/java/org/oasis_eu/portal/back/content/ContentController.java
@@ -15,22 +15,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * XXX hardcoded content/home edition references
  * 
  * @author mkalamalami
  *
  */
 @Controller
 public class ContentController extends PortalController {
-	
-    public static final String CONTENTS_PATH = "/contents";
     
     public static final String DEFAULT_CONTENT = "home";
 
@@ -40,23 +39,41 @@ public class ContentController extends PortalController {
     @Autowired
     private ContentItemRepository repository;
     
-    /**
-     * The language that will be displayed by default in the editor.
-     */
-    private Languages defaultLanguage = null;
+    @Autowired
+    private HttpServletRequest request;
     
-    @RequestMapping(CONTENTS_PATH)
+    private Languages defaultEditorLanguage = null;
+    
+    @RequestMapping("/contents")
     public String edit(Model model, HttpServletRequest request) {
-        return showContentsEditor(null, model, request);
+        return "redirect:/contents/edit/" + DEFAULT_CONTENT;
     }
     
-    @RequestMapping(CONTENTS_PATH + "/edit/{contentId}")
-    public String edit(@PathVariable String contentId, Model model, HttpServletRequest request) {
-        return showContentsEditor(contentId, model, request);
+    @RequestMapping("/contents/edit/{contentId}")
+    public String edit(@PathVariable String contentId, Model model) {
+        contentId = ((contentId != null) ? contentId : DEFAULT_CONTENT);
+        ContentItemInfo contentItemStats = new ContentItemInfo(repository.findOne(contentId));
+        
+        model.addAttribute("navigation", backendNavigationService.getNavigation("contents"));
+        model.addAttribute("pageTemplate", "contents");
+        model.addAttribute("contentList", getContentList());
+        
+        if (contentItemStats.getContentItem() != null) {
+			model.addAttribute("currentContent", contentItemStats.getContentItem());
+	        model.addAttribute("missingTranslations", contentItemStats.getMissingTranslations());
+        }
+        
+        return "backend";
     }
     
-    @RequestMapping(value = CONTENTS_PATH + "/create", method = RequestMethod.POST)
-    public String create(@RequestParam String contentId, Model model) {
+    @RequestMapping(value = "/contents/save", method = RequestMethod.POST)
+	public String save(@RequestBody ContentItem contentItem) {
+	    repository.save(contentItem);
+	    return "redirect:/contents/edit/" + contentItem.getId();
+	}
+
+	@RequestMapping(value = "/contents/create", method = RequestMethod.POST)
+    public String create(@RequestParam String contentId) {
         if (!StringUtils.isEmpty(contentId)) {
             ContentItem newContentItem = new ContentItem();
             newContentItem.setId(contentId);
@@ -66,46 +83,62 @@ public class ContentController extends PortalController {
         return "redirect:/contents";
     }
     
-    @RequestMapping(value = CONTENTS_PATH + "/save", method = RequestMethod.POST)
-    public String save(@RequestBody ContentItem contentItem, Model model) {
-        repository.save(contentItem);
-        return "redirect:/contents/edit/" + contentItem.getId();
+    @RequestMapping(value = "/contents/rename", method = RequestMethod.POST)
+    public String rename(@RequestParam("oldName") String oldName,
+    		@RequestParam("newName") String newName) {
+        if (!DEFAULT_CONTENT.equals(oldName)) {
+	        ContentItem item = repository.findOne(oldName);
+	        ContentItem existingItem = repository.findOne(newName);
+	        if (existingItem == null) {
+		        repository.delete(item);
+		        item.setId(newName);
+		        repository.save(item);
+		        return "redirect:/contents/edit/" + newName;
+	        }
+	        else {
+	        	// TODO Show error
+		        return "redirect:/contents/edit/" + oldName;
+	        }
+        }
+        else {
+            return "redirect:/contents";
+        }
     }
-
-    @RequestMapping(value = CONTENTS_PATH + "/delete/{contentId}", method = RequestMethod.POST)
-    public String delete(@PathVariable String contentId, Model model) {
+    
+    @RequestMapping(value = "/contents/delete/{contentId}", method = RequestMethod.POST)
+    public String delete(@PathVariable String contentId) {
         if (!DEFAULT_CONTENT.equals(contentId)) {
             repository.delete(contentId);
         }
-        return "redirect:/contents/edit/" + DEFAULT_CONTENT;
+        return "redirect:/contents";
     }
 
-    @RequestMapping(value = CONTENTS_PATH + "/defaultlanguage", method = RequestMethod.POST)
-    public String changeDefaultLanguage(@RequestBody String defaultLanguage, Model model) {
+    @RequestMapping(value = "/contents/defaultlanguage", method = RequestMethod.POST)
+    @ResponseBody
+    public void changeDefaultEditorLanguage(@RequestBody String defaultEditorLanguageName) {
         for (Languages language : Languages.values()) {
-            if (language.getLocale().getLanguage().equals(defaultLanguage)) {
-                this.defaultLanguage = language;
+            if (language.getLocale().getLanguage().equals(defaultEditorLanguageName)) {
+                this.defaultEditorLanguage = language;
                 break;
             }
         }
-        return "empty"; // XXX Cleaner way to return nothing?
-    }
-    
-    public String showContentsEditor(String contentId, Model model, HttpServletRequest request) {
-        contentId = ((contentId != null) ? contentId : DEFAULT_CONTENT);
-        ContentItemInfo contentItemStats = new ContentItemInfo(repository.findOne(contentId));
-        
-        model.addAttribute("navigation", backendNavigationService.getNavigation("contents"));
-        model.addAttribute("pageTemplate", "contents");
-        model.addAttribute("contentList", getContentList());
-		model.addAttribute("currentContent", contentItemStats.getContentItem());
-        model.addAttribute("missingTranslations", contentItemStats.getMissingTranslations());
-        model.addAttribute("languagesCount", Languages.values().length);
-        model.addAttribute("defaultLanguage", defaultLanguage != null ? defaultLanguage : currentLanguage(request));
-        
-        return "backend";
     }
 
+    @ModelAttribute("defaultEditorLanguage")
+    public Languages getDefaultEditorLanguage() {
+    	return defaultEditorLanguage != null ? defaultEditorLanguage : currentLanguage(request);
+	}
+    
+    @ModelAttribute("languagesCount")
+    public int getLanguagesCount() {
+    	return Languages.values().length;
+	}
+
+    
+
+    
+    
+    
     private List<ContentItemInfo> getContentList() {
         // FIXME Bad performance
         // Use clever mongo query, or simply store missing translations count in content item?
