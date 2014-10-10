67d51a2a2c0e834ad3fa7abdcf7d6861aebd820e
Sylvain Chambon <sylvain.chambon@openwide.fr>
2014-10-10 09:03:43 +0200

    Precompile JSX for non-devmode

--- portal-parent/oasis-portal-front/src/main/resources/public/js/notifications.jsx.js
+++ portal-parent/oasis-portal-front/src/main/resources/public/js/notifications.jsx.js
@@ -11,7 +11,7 @@ function t(key) {
 
 
 
-var NotificationTable = React.createClass({displayName: 'NotificationTable',
+var NotificationTable = React.createClass({
     loadNotifications: function() {
         $.ajax({
             url: this.props.url,
@@ -61,25 +61,25 @@ var NotificationTable = React.createClass({displayName: 'NotificationTable',
         var callback = this.removeNotif;
         if (this.state.n.length == 0) {
             return (
-                React.DOM.div({className: "standard-form"}, 
-                t('no-notification')
-                )
+                <div className="standard-form">
+                {t('no-notification')}
+                </div>
                 );
         } else {
             var notificationNodes = this.state.n.map(function (notif) {
                 return (
-                    Notification({key: notif.id, notif: notif, onRemoveNotif: callback})
+                    <Notification key={notif.id} notif={notif} onRemoveNotif={callback}/>
                     );
             });
             return (
-                React.DOM.div({className: "standard-form"}, 
-                    React.DOM.div({className: "row form-table-header"}, 
-                        React.DOM.div({className: "col-sm-2", onClick: this.sortBy('date')}, t('date')), 
-                        React.DOM.div({className: "col-sm-2", onClick: this.sortBy('appName')}, t('app')), 
-                        React.DOM.div({className: "col-sm-6", onClick: this.sortBy('formattedText')}, t('message'))
-                    ), 
-                notificationNodes
-                )
+                <div className="standard-form">
+                    <div className="row form-table-header">
+                        <div className="col-sm-2" onClick={this.sortBy('date')}>{t('date')}</div>
+                        <div className="col-sm-2" onClick={this.sortBy('appName')}>{t('app')}</div>
+                        <div className="col-sm-6" onClick={this.sortBy('formattedText')}>{t('message')}</div>
+                    </div>
+                {notificationNodes}
+                </div>
                 );
         }
     }
@@ -92,20 +92,20 @@ var Notification = React.createClass({
     },
     render: function() {
         return (
-            React.DOM.div({className: "row form-table-row"}, 
-                React.DOM.div({className: "col-sm-2"}, this.props.notif.dateText), 
-                React.DOM.div({className: "col-sm-2"}, this.props.notif.appName), 
-                React.DOM.div({className: "col-sm-6", dangerouslySetInnerHTML: {__html: this.props.notif.formattedText}}), 
-                React.DOM.div({className: "col-sm-2"}, 
-                    React.DOM.a({href: this.props.notif.url, target: "_new", className: "btn btn-primary"}, t('manage')), 
-                    React.DOM.a({href: "#", className: "btn btn-primary", onClick: this.removeNotif}, t('archive'))
-                )
-            )
+            <div className="row form-table-row">
+                <div className="col-sm-2">{this.props.notif.dateText}</div>
+                <div className="col-sm-2">{this.props.notif.appName}</div>
+                <div className="col-sm-6" dangerouslySetInnerHTML={{__html: this.props.notif.formattedText}}></div>
+                <div className="col-sm-2">
+                    <a href={this.props.notif.url} target="_new" className="btn btn-primary" >{t('manage')}</a>
+                    <a href="#" className="btn btn-primary" onClick={this.removeNotif}>{t('archive')}</a>
+                </div>
+            </div>
             );
     }
 });
 
 
 React.renderComponent(
-    NotificationTable({url: notificationService, pollInterval: 2000})
+    <NotificationTable url={notificationService} pollInterval={2000}/>
     , document.getElementById("notifications"));
