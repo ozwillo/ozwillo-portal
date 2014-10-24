866e02689f65479d6d809f92588fc7af24c654e3
Sylvain Chambon <sylvain.chambon@openwide.fr>
2014-10-24 09:22:50 +0200

    Don't just compile jsx->js but minify as well

--- jsx.py
+++ jsx.py
@@ -1,10 +1,16 @@
 #! /usr/bin/python3
+# Compile JSX & minify javascript
+# Usage of compiled / minified JS is turned on with the "devmode" flag in the Portal
+# Note that this requires Node and the following two NPM modules:
+# - jsx
+# - uglify-js (installed with npm install uglifyjs -g to provide the command line wrapper)
 
 import os
 import re
 
-f = os.popen('find . -name "*.jsx.js"')
+f = os.popen('find . -wholename "*/src/*.jsx.js"')
 
 for line in f:
-  print("Compiling: " + line.strip())
-  os.system("jsx " + line.strip() + " > " + re.sub("\.jsx","",line.strip()))
+  l = line.strip()
+  print("Compiling: " + l)
+  os.system("jsx " + l + " | uglifyjs - > " + re.sub("\.jsx",".min",l))
