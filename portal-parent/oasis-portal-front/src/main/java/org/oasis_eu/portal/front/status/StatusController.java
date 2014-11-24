811a94be97beb13e98980875519615b4f621eb17
Sylvain Chambon <sylvain.chambon@openwide.fr>
2014-11-24 12:46:53 +0100

    Do not try to fetch app info from the kernel in the status endpoint

--- portal-parent/oasis-portal-front/src/main/java/org/oasis_eu/portal/front/status/StatusController.java
+++ portal-parent/oasis-portal-front/src/main/java/org/oasis_eu/portal/front/status/StatusController.java
@@ -1,7 +1,6 @@
 package org.oasis_eu.portal.front.status;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.oasis_eu.portal.core.dao.CatalogStore;
 import org.oasis_eu.portal.core.mongo.dao.my.DashboardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,22 +17,17 @@ public class StatusController {
     @Autowired
     private DashboardRepository dashboardRepository;
 
-    @Autowired
-    private CatalogStore catalogStore;
-
     @RequestMapping
     public Status status() {
         Status status = new Status();
 
         status.databaseOk = dashboardRepository.count() != 0;
-        status.kernelOk = catalogStore.findApplication("portal") != null;
 
         return status;
     }
 
     public static class Status {
         @JsonProperty boolean databaseOk;
-        @JsonProperty boolean kernelOk;
     }
 
 }
