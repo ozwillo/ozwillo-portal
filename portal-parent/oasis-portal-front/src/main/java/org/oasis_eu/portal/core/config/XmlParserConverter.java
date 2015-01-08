f0d2c7eb889387dc583be68483c136d2568e8f1d
Sylvain Chambon <sylvain.chambon@openwide.fr>
2015-01-08 19:04:49 +0100

    Reload site map from www (every 15 minutes by default) Close #131

---
+++ portal-parent/oasis-portal-front/src/main/java/org/oasis_eu/portal/core/config/XmlParserConverter.java
@@ -0,0 +1,42 @@
+package org.oasis_eu.portal.core.config;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * User: schambon
+ * Date: 1/8/15
+ */
+public class XmlParserConverter extends AbstractHttpMessageConverter<Object> {
+
+    private XmlMapper xmlMapper = new XmlMapper();
+
+    @Override
+    public List<MediaType> getSupportedMediaTypes() {
+        return Arrays.asList(MediaType.APPLICATION_XML);
+    }
+
+    @Override
+    protected boolean supports(Class<?> clazz) {
+        return true;
+    }
+
+    @Override
+    protected Object readInternal(Class<?> clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
+        return xmlMapper.readValue(inputMessage.getBody(), clazz);
+    }
+
+    @Override
+    protected void writeInternal(Object o, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
--- portal-parent/oasis-portal-front/src/main/java/org/oasis_eu/portal/core/mongo/model/sitemap/SiteMapEntry.java
+++ portal-parent/oasis-portal-front/src/main/java/org/oasis_eu/portal/core/mongo/model/sitemap/SiteMapEntry.java
@@ -1,5 +1,8 @@
 package org.oasis_eu.portal.core.mongo.model.sitemap;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText;
+
 import java.io.Serializable;
 
 /**
@@ -7,9 +10,10 @@ import java.io.Serializable;
  * Date: 12/15/14
  */
 public class SiteMapEntry implements Serializable {
-
-    private String url;
-    private String label;
+    @JacksonXmlProperty(localName = "href", isAttribute = true)
+    private String url = "";
+    @JacksonXmlText
+    private String label = "";
 
     public String getUrl() {
         return url;
