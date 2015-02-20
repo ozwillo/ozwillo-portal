a13d654821f5772f79f719d5b4c09ade0fa57de6
Marc Dutoo <marc.dutoo@openwide.fr>
2015-02-20 14:48:42 +0100

    removed react.js src/.module-cache/ temp files unduly committed

--- portal-parent/oasis-portal-front/src/.module-cache/1e93baf932ea265807da118401292e708e66869e.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-    $("div#veil").click(function() {
-        window.location = $(this).find("a").attr("href");
-    });
-
-
-    setupAppPage();
-});
--- portal-parent/oasis-portal-front/src/.module-cache/3076700001593cd75ed6d6975890f0963207f563.js
+++
@@ -1,9 +0,0 @@
-/* init csrf prevention */
-$(function () {
-    var token = $("meta[name='_csrf']").attr("content");
-    var header = $("meta[name='_csrf_header']").attr("content");
-    $(document).ajaxSend(function (e, xhr, options) {
-        xhr.setRequestHeader(header, token);
-    });
-});
-
