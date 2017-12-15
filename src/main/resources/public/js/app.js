c431e3de2cf2bbf635a69cafd09dd386fd6594e0
Nicoletti Sébastien <sebastien.nicoletti@inotekk.com>
2017-12-15 15:06:36 +0100

    nav bar: fix navigation to change page

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -2,18 +2,12 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './config/router';
 
-//Load redux
-
-//Components
-import Layout from './components/layout';
 
 class App extends React.Component {
 
     render() {
         return <BrowserRouter>
-            <Layout>
-                <Router/>
-            </Layout>
+            <Router/>
         </BrowserRouter>;
     }
 
--- src/main/resources/public/js/config/router.js
+++ src/main/resources/public/js/config/router.js
@@ -4,6 +4,8 @@ import { Router, Switch } from 'react-router';
 
 import history from './history';
 
+import Layout from '../components/layout';
+
 //Pages
 import Dashboard from '../pages/dashboard';
 import Profile from '../pages/profile';
@@ -11,10 +13,12 @@ import Profile from '../pages/profile';
 class AppRouter extends React.Component {
     render() {
         return <Router history={history}>
-            <Switch>
-                <Route path="/(my/)?(dasboard)?" component={Dashboard}/>
-                <Route path="/my/profile" component={Profile}/>
-            </Switch>
+            <Layout>
+                <Switch>
+                    <Route path="/my/profile" component={Profile}/>
+                    <Route path="/(my/)?(dashboard)?" component={Dashboard}/>
+                </Switch>
+            </Layout>
         </Router>;
     }
 }
