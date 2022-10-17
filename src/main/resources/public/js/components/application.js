fced928721c66ea359c5f004d3da556c59ce8f82
farah <farah.fournat@gmail.com>
2022-10-17 11:37:39 +0200

    less apps on a single line

--- src/main/resources/public/js/components/application.js
+++ src/main/resources/public/js/components/application.js
@@ -25,9 +25,7 @@ class Application extends React.PureComponent {
       : "free";
 
     return (
-      <div
-        className={`col-lg-2 col-md-3 col-sm-4 col-xs-6 container-app ${indicatorStatus}-app`}
-      >
+      <div className={`col-6 col-sm-4 container-app ${indicatorStatus}-app`}>
         <div className="app" onClick={() => this.openInstallAppPage()}>
           <div className="logo">
             <img src={app.icon} />
