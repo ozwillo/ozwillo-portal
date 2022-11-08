a3cd8e0c0aa306352a3dffc1977bc6cfa719d760
farah <farah.fournat@gmail.com>
2022-11-08 14:30:42 +0100

    prettierrc update

--- src/main/resources/public/js/components/application.js
+++ src/main/resources/public/js/components/application.js
@@ -1,122 +1,94 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
-import { i18n } from "../config/i18n-config";
-import { t } from "@lingui/macro";
-import ReactTooltip from "react-tooltip";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { i18n } from '../config/i18n-config';
+import { t } from '@lingui/macro';
+import ReactTooltip from 'react-tooltip';
 
 class Application extends React.PureComponent {
-  openInstallAppPage = () => {
-    const { app, config } = this.props;
-    const installAppPage = `/${config.language}/store/${app.type}/${app.id}`;
-    this.props.history.push({
-      pathname: installAppPage,
-      state: { app: app, config: config },
-    });
-  };
+    openInstallAppPage = () => {
+        const { app, config } = this.props;
+        const installAppPage = `/${config.language}/store/${app.type}/${app.id}`;
+        this.props.history.push({
+            pathname: installAppPage,
+            state: { app: app, config: config },
+        });
+    };
 
-  render = () => {
-    const { app } = this.props;
+    render = () => {
+        const { app } = this.props;
 
-    const indicatorStatus = app.installed
-      ? "installed"
-      : app.paid
-      ? "paid"
-      : "free";
+        const indicatorStatus = app.installed ? 'installed' : app.paid ? 'paid' : 'free';
 
-    return (
-      <div className={`col-6 col-sm-4 container-app ${indicatorStatus}-app`}>
-        <div className="app" onClick={() => this.openInstallAppPage()}>
-          <div className="logo">
-            <img src={app.icon} />
-          </div>
-          <div className="description">
-            <div className="app-header">
-              <span className="app-name" data-tip data-for={app.id}>
-                {app.name}
-              </span>
-              <ReactTooltip id={app.id} place="top" effect="solid">
-                <span>{app.name}</span>
-              </ReactTooltip>
-              <p className="app-provider">{app.provider}</p>
+        return (
+            <div className={`col-6 col-sm-4 container-app ${indicatorStatus}-app`}>
+                <div className="app" onClick={() => this.openInstallAppPage()}>
+                    <div className="logo">
+                        <img src={app.icon} />
+                    </div>
+                    <div className="description">
+                        <div className="app-header">
+                            <span className="app-name" data-tip data-for={app.id}>
+                                {app.name}
+                            </span>
+                            <ReactTooltip id={app.id} place="top" effect="solid">
+                                <span>{app.name}</span>
+                            </ReactTooltip>
+                            <p className="app-provider">{app.provider}</p>
+                        </div>
+                        <p className="app-description">{app.description}</p>
+                        <Indicator status={indicatorStatus} />
+                    </div>
+                </div>
             </div>
-            <p className="app-description">{app.description}</p>
-            <Indicator status={indicatorStatus} />
-          </div>
-        </div>
-      </div>
-    );
-  };
+        );
+    };
 }
 
 Application.propTypes = {
-  app: PropTypes.object,
-  config: PropTypes.object,
+    app: PropTypes.object,
+    config: PropTypes.object,
 };
 
 export default withRouter(Application);
 
 export class Indicator extends React.PureComponent {
-  render() {
-    let btns;
-    const status = this.props.status;
-    if (status === "installed") {
-      btns = [
-        <button
-          type="button"
-          key="indicator_button"
-          className="btn btn-lg btn-installed"
-        >
-          {i18n._(t`store.installed`)}
-        </button>,
-        <button
-          type="button"
-          key="indicator_icon"
-          className="btn btn-lg btn-installed-indicator"
-        >
-          <i className="fa fa-check" />
-        </button>,
-      ];
-    } else if (status === "free") {
-      btns = [
-        <button
-          type="button"
-          key="indicator_button"
-          className="btn btn-lg btn-free"
-        >
-          {i18n._(t`store.free`)}
-        </button>,
-        <button
-          type="button"
-          key="indicator_icon"
-          className="btn btn-lg btn-free-indicator"
-        >
-          <i className="fa fa-gift" />
-        </button>,
-      ];
-    } else {
-      btns = [
-        <button
-          type="button"
-          key="indicator_button"
-          className="btn btn-lg btn-buy"
-        >
-          {i18n._(t`store.paid`)}
-        </button>,
-        <button
-          type="button"
-          key="indicator_icon"
-          className="btn btn-lg btn-buy-indicator"
-        >
-          <i className="fa fas fa-euro-sign" />
-        </button>,
-      ];
-    }
+    render() {
+        let btns;
+        const status = this.props.status;
+        if (status === 'installed') {
+            btns = [
+                <button type="button" key="indicator_button" className="btn btn-lg btn-installed">
+                    {i18n._(t`store.installed`)}
+                </button>,
+                <button type="button" key="indicator_icon" className="btn btn-lg btn-installed-indicator">
+                    <i className="fa fa-check" />
+                </button>,
+            ];
+        } else if (status === 'free') {
+            btns = [
+                <button type="button" key="indicator_button" className="btn btn-lg btn-free">
+                    {i18n._(t`store.free`)}
+                </button>,
+                <button type="button" key="indicator_icon" className="btn btn-lg btn-free-indicator">
+                    <i className="fa fa-gift" />
+                </button>,
+            ];
+        } else {
+            btns = [
+                <button type="button" key="indicator_button" className="btn btn-lg btn-buy">
+                    {i18n._(t`store.paid`)}
+                </button>,
+                <button type="button" key="indicator_icon" className="btn btn-lg btn-buy-indicator">
+                    <i className="fa fas fa-euro-sign" />
+                </button>,
+            ];
+        }
 
-    return <div className="app-status text-center">{btns}</div>;
-  }
+        return <div className="app-status text-center">{btns}</div>;
+    }
 }
 
 Indicator.propTypes = {
-  status: PropTypes.string,
+    status: PropTypes.string,
 };
--- src/main/resources/public/js/components/catalog-card.js
+++ src/main/resources/public/js/components/catalog-card.js
@@ -1,139 +1,132 @@
-import React from "react";
-import "../../css/components/catalog-card.css";
-import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
-import { i18n } from "../config/i18n-config";
-import { t } from "@lingui/macro";
-import LabelSection from "./label-section";
+import React from 'react';
+import '../../css/components/catalog-card.css';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { i18n } from '../config/i18n-config';
+import { t } from '@lingui/macro';
+import LabelSection from './label-section';
 
 class CatalogCard extends React.PureComponent {
-  openInstallAppPage = () => {
-    const { app, config } = this.props;
-    const installAppPage = `/${config.language}/store/${app.type}/${app.id}`;
-    this.props.history.push({
-      pathname: installAppPage,
-      state: { app: app, config: config },
-    });
-  };
+    openInstallAppPage = () => {
+        const { app, config } = this.props;
+        const installAppPage = `/${config.language}/store/${app.type}/${app.id}`;
+        this.props.history.push({
+            pathname: installAppPage,
+            state: { app: app, config: config },
+        });
+    };
 
-  getTags = (app) => {
-    let buttons = [];
+    getTags = app => {
+        let buttons = [];
 
-    if (app.installed) {
-      buttons.push({
-        className: "fa fa-check category-icon",
-        text: i18n._(t`store.installed`),
-      });
-    }
+        if (app.installed) {
+            buttons.push({
+                className: 'fa fa-check category-icon',
+                text: i18n._(t`store.installed`),
+            });
+        }
 
-    if (app.paid) {
-      buttons.push({
-        className: "fa fas fa-euro-sign category-icon",
-        text: i18n._(t`store.paid`),
-      });
-    } else {
-      buttons.push({
-        className: "fa fa-gift category-icon",
-        text: i18n._(t`store.free`),
-      });
-    }
+        if (app.paid) {
+            buttons.push({
+                className: 'fa fas fa-euro-sign category-icon',
+                text: i18n._(t`store.paid`),
+            });
+        } else {
+            buttons.push({
+                className: 'fa fa-gift category-icon',
+                text: i18n._(t`store.free`),
+            });
+        }
 
-    const classNameButton = "audience-btn";
-    if (app.target_citizens) {
-      buttons.push({
-        className: "fa fa-users category-icon",
-        text: i18n._(t`store.citizens`),
-        classNameButton,
-      });
-    }
+        const classNameButton = 'audience-btn';
+        if (app.target_citizens) {
+            buttons.push({
+                className: 'fa fa-users category-icon',
+                text: i18n._(t`store.citizens`),
+                classNameButton,
+            });
+        }
 
-    if (app.target_companies) {
-      buttons.push({
-        className: "fa fa-briefcase category-icon",
-        text: i18n._(t`store.companies`),
-        classNameButton,
-      });
-    }
+        if (app.target_companies) {
+            buttons.push({
+                className: 'fa fa-briefcase category-icon',
+                text: i18n._(t`store.companies`),
+                classNameButton,
+            });
+        }
 
-    if (app.target_publicbodies) {
-      buttons.push({
-        className: "fa fa-building category-icon",
-        text: i18n._(t`store.publicbodies`),
-        classNameButton,
-      });
-    }
+        if (app.target_publicbodies) {
+            buttons.push({
+                className: 'fa fa-building category-icon',
+                text: i18n._(t`store.publicbodies`),
+                classNameButton,
+            });
+        }
 
-    return buttons;
-  };
+        return buttons;
+    };
 
-  render = () => {
-    const { app } = this.props;
-    return (
-      <div className="content-wrapper">
-        <div
-          className="cards-container"
-          onClick={() => this.openInstallAppPage()}
-        >
-          <div id="card" onClick={() => this.openInstallAppPage()}>
-            <div id="main">
-              <div id="icon-wrapper">
-                <img src={app.icon} />
-              </div>
-              <div id="content">
-                <div className="bloc">
-                  <div id="app-name">
-                    <div id="name" className="flex-text">
-                      {app.name}
+    render = () => {
+        const { app } = this.props;
+        return (
+            <div className="content-wrapper">
+                <div className="cards-container" onClick={() => this.openInstallAppPage()}>
+                    <div id="card" onClick={() => this.openInstallAppPage()}>
+                        <div id="main">
+                            <div id="icon-wrapper">
+                                <img src={app.icon} />
+                            </div>
+                            <div id="content">
+                                <div className="bloc">
+                                    <div id="app-name">
+                                        <div id="name" className="flex-text">
+                                            {app.name}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="app-provider">{app.provider}</div>
+                                <div id="app-description">{app.description}</div>
+                            </div>
+                        </div>
+                        <div id="footer">
+                            <div className="footer-infos">
+                                {this.getTags(app).map(tagInfo => (
+                                    <Tag
+                                        className={tagInfo.className}
+                                        text={tagInfo.text}
+                                        classNameButton={tagInfo.classNameButton}
+                                    />
+                                ))}
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-                <div id="app-provider">{app.provider}</div>
-                <div id="app-description">{app.description}</div>
-              </div>
             </div>
-            <div id="footer">
-              <div className="footer-infos">
-                {this.getTags(app).map((tagInfo) => (
-                  <Tag
-                    className={tagInfo.className}
-                    text={tagInfo.text}
-                    classNameButton={tagInfo.classNameButton}
-                  />
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  };
+        );
+    };
 }
 
 CatalogCard.propTypes = {
-  app: PropTypes.object,
-  config: PropTypes.object,
+    app: PropTypes.object,
+    config: PropTypes.object,
 };
 
 export default withRouter(CatalogCard);
 
 export class Tag extends React.PureComponent {
-  render() {
-    const { className, text, classNameButton } = this.props;
+    render() {
+        const { className, text, classNameButton } = this.props;
 
-    return (
-      <button
-        type="button"
-        key="indicator_button"
-        className={`category-btn ${classNameButton}`}
-      >
-        <i className={className}></i>
-        <span className="btn-label">{text}</span>
-      </button>
-    );
-  }
+        return (
+            <button type="button" key="indicator_button" className={`category-btn ${classNameButton}`}>
+                <i className={className}></i>
+                <span className="btn-label">{text}</span>
+            </button>
+        );
+    }
 }
 Tag.propTypes = {
-  className: PropTypes.string,
-  text: PropTypes.string,
-  classNameButton: PropTypes.string,
+    className: PropTypes.string,
+    text: PropTypes.string,
+    classNameButton: PropTypes.string,
 };
